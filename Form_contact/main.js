eb3d3fcb0aacf224b6090d2e475eece2617eea33
Arretado Labs <fgsj.dev@outlook.com>
2023-12-01 07:57:00 -0300

    File modified for adding lines comments explanation code

--- Form_contact/main.js
+++ Form_contact/main.js
@@ -1,9 +1,25 @@
+/**
+ * Essa função é referente ao serviço de envio de email
+ * primeiramente dando valores aos parâmetros "puxando" 
+ * os identificadores nos "ids" no arquivo index.html
+ * assim no último bloco de código, já iniciando o código
+ * com o serviço do emailjs e usando os parâmetros que criamos
+ * Podemos digamos "empacotar" aonde o cada valor está em 
+ * um campo do formulário.
+ */
+
+
 function sendMail() {
     var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value,
     };
+
+    /**
+     *duas variáveis constantes que correspondem as chaves geradas
+      do serviço de email e do template utilizado 
+     */ 
     const serviceID = "service_t9p4wmd";
     const templateID = "template_0w4be6n";
 
